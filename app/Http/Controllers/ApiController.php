99707afc0c50e728f18eb8a23fd08b210c8219ca
Dragordc <190103181@stu.sdu.edu.kz>
2021-03-20 18:29:08 +0600

    All changes

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class ApiController extends Controller
+{
+    //
+	public function index(){
+		$posts = Post::all();
+
+		return response($posts,200);
+	}
+	public function get_id(Request $request){
+         $posts = Post::find($request->post_id);
+
+         if($posts == null) {
+             return response(['message'=>'There is no post'],404);
+         }
+         return response($posts,200);
+	}
+}
